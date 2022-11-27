8bf22ec022ee6145fb736c575631a7fede472eb6
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-11-27 16:55:23 +0300

    Added representation level.

---
+++ DirectoryScanner/BoolToImageConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace DirectoryScannerConverters
+{ 
+     public class BoolToImageConverter : IValueConverter
+     {
+            public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                bool val = (bool)value;
+                if (!val) //is file
+                {
+                    // TODO I.V. refactor path.
+                    return new BitmapImage(new Uri(@"C:\Users\Lenovo\Documents\бгуир\предметы\3курс\5сем\СПП\лабы\3 лаба\DirectoryScanner\DirectoryScanner\Assets\file.png", UriKind.RelativeOrAbsolute));
+                }
+                else //is directory
+                {
+                    return new BitmapImage(new Uri(@"C:\Users\Lenovo\Documents\бгуир\предметы\3курс\5сем\СПП\лабы\3 лаба\DirectoryScanner\DirectoryScanner\Assets\directory.png", UriKind.RelativeOrAbsolute));
+                }
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+     }  
+}
---
+++ DirectoryScanner/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DirectoryScannerLibrary;
+using Microsoft.Win32;
+using Microsoft.Windows.Themes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using DirectoryScannerConverters;
+
+using static System.Net.WebRequestMethods;
+
+namespace DirectoryScanner
+{
+    public partial class MainWindow : Window
+    {
+        public ViewModel viewModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+            viewModel = new ViewModel();
+            treeView1.ItemsSource = viewModel.directoryTracer.Files;
+
+            this.DataContext = viewModel;
+        }
+        
+    }
+}
